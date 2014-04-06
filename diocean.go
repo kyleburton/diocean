6191580d151dc8d41fda09e22ee35c6a555d0778
Kyle Burton <kyle.burton@gmail.com>
2014-04-06 11:28:05 -0400

    separate library

--- diocean.go
+++ diocean.go
@@ -4,38 +4,38 @@ import (
   "flag"
   "os"
   "fmt"
-  "github.com/kyleburton/diocean"
+  "github.com/kyleburton/diocean-go"
 )
 
 func main() {
 	flag.BoolVar(&diocean.CmdlineOptions.Verbose, "v", false, "Verbose")
 	flag.BoolVar(&diocean.CmdlineOptions.WaitForEvent, "w", false, "For commands that return an event_id, wait for the event to complete.")
-	InitRoutingTable()
+	diocean.InitRoutingTable()
 	flag.Parse()
-	route := FindMatchingRoute(flag.Args())
-	if CmdlineOptions.Verbose {
+	route := diocean.FindMatchingRoute(flag.Args())
+	if diocean.CmdlineOptions.Verbose {
 		fmt.Fprintf(os.Stderr, "Args: %s\n", flag.Args())
 	}
-	if CmdlineOptions.Verbose {
+	if diocean.CmdlineOptions.Verbose {
 		fmt.Fprintf(os.Stderr, "Route: %s\n", route)
 	}
 
 	if route == nil {
 		fmt.Fprintf(os.Stderr, "Error: unrecognized command: %s\n", flag.Args())
-		ShowGeneralHelp(route)
+		diocean.ShowGeneralHelp(route)
 		os.Exit(1)
 	}
 
-	if !InitConfig() {
+	if !diocean.InitConfig() {
 		fmt.Fprintf(os.Stderr, "Invalid or Missing configuration file.\n")
 		os.Exit(1)
 	}
-	if CmdlineOptions.Verbose {
-		fmt.Fprintf(os.Stderr, "Config: %s\n", Config)
+	if diocean.CmdlineOptions.Verbose {
+		fmt.Fprintf(os.Stderr, "Config: %s\n", diocean.Config)
 	}
 
 	if route != nil {
-		if CmdlineOptions.Verbose {
+		if diocean.CmdlineOptions.Verbose {
 			fmt.Fprintf(os.Stderr, "Calling route: %s\n", route)
 		}
 		route.Handler(route)
